8d57c7f5ac15fe4084177c895f49da4fc5128ff0
Mihai Cozmuța <mihaicozmuta@Mihais-MacBook-Pro.local>
2020-11-26 15:07:27 +0200

    added validation for email

--- backend/tracker/locations/models.py
+++ backend/tracker/locations/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
 from rest_framework import serializers
-from locations.validations import name_validator
+from locations.validations import name_validator, mail_validator
 
 #the model which contains all the user details that we need
 class Client(models.Model):
     first_name = models.CharField(max_length=100, validators=[name_validator])
     last_name = models.CharField(max_length=100, validators=[name_validator])
-    email = models.CharField(max_length=100, unique=True)
+    email = models.CharField(max_length=100, unique=True, validators=[mail_validator])
     password = User.password
     created_at = models.DateTimeField(auto_now_add=True)
--- backend/tracker/locations/validations.py
+++ backend/tracker/locations/validations.py
@@ -2,14 +2,14 @@ from rest_framework import serializers
 
 
 
-
-def latitude_restriction(lat):
-    if lat > 20:
-        raise serializers.ValidationError("the latitude should be smaller than 20")
-    return lat
-
-
 def name_validator(name:str):
     if name is None:
         raise serializers.ValidationError("The name cannot be empty")
     return name
+
+def mail_validator(mail:str):
+    if '@' not in mail:
+        raise serializers.ValidationError('This is not a vaild e-mail')
+    if '.com' or '.ro' not in mail:
+        raise serializers.ValidationError('This is not a valid e-mail')
+    return mail 
